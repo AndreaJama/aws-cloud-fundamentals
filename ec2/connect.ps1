e443c23df04e782c851a4dc6df8beeb7380ecf94
Andrea Jama <142735173+AndreaJama@users.noreply.github.com>
2025-11-28 11:05:19 -0300

    Update connect.ps1

--- ec2/connect.ps1
+++ ec2/connect.ps1
@@ -1,18 +1,18 @@
 
-## 🔧 Scripts usados
+## Scripts usados
 
 ### PowerShell — Ajuste de permissões e SSH
-
-```powershell
 
 # cd até a pasta onde está a chave .pem (no me caso está em Downloads)
 cd .\Downloads
 
-# Remove permissões herdadas
-icacls "C:\Users\AndreaJ\Downloads\treinoweb-maquinas.pem" /inheritance:r
-
-# Concede permissão somente ao usuário atual USERNAME=AndreaJ
-icacls "C:\Users\AndreaJ\Downloads\treinoweb-maquinas.pem" /grant:r AndreaJ:R
-
 # Conectar via SSH. Modifique O IP se necessário
 ssh -i .\treinoweb-maquinas.pem ubuntu@54.94.132.202
+
+yes
+
+### Instalação do Nginx
+
+sudo apt-get install nginx
+
+y
